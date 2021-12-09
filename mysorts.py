b9e747c6a7cdbce4eba801c666593f80d284d295
DVDBin <38662376+sibin57@users.noreply.github.com>
2021-12-09 22:12:37 +0300

    applied changes, according to pep 8
    wraped lines under 79 characters, added spaces arround operators and changed comments a little

--- mysorts.py
+++ mysorts.py
@@ -4,7 +4,7 @@ def bubble_sort(lst):
     while had_swaps:
         had_swaps = False
         for i in range(1, len(lst)):
-            if lst[i]<lst[i-1]:
+            if lst[i] < lst[i-1]:
                 lst[i], lst[i-1] = lst[i-1], lst[i]
                 had_swaps = True
 
@@ -12,15 +12,15 @@ def bubble_sort(lst):
 def selection_sort(lst):
     for i in range(len(lst)-1, -1, -1):
         for j in range(i, -1, -1):
-            if lst[j]>lst[i]:
+            if lst[j] > lst[i]:
                 lst[i], lst[j] = lst[j], lst[i]
 
 
 def insertion_sort(lst):
     for i in range(1, len(lst)):
         elem = lst[i]
-        j=i
-        while j>0 and lst[j-1]>elem:
+        j = i
+        while j > 0 and lst[j-1]>elem:
             lst[j] = lst[j-1]
-            j-=1
-        lst[j]=elem
+            j -= 1
+        lst[j] = elem
--- sorting.py
+++ sorting.py
@@ -1,21 +1,31 @@
-####################################
-#изначальный набор комманд:
-#1 загрузить список в панель
-#2 отсортировать список пузырьком
-#3 отсортировать список выбором
-#4 отсортировать список вставками
-#5 вывести полученый список в панель
-#0 выход
-#TODO pep 8; pep 257; расширить загрузку файла, чтоб можно было подавать списки и через пробел;добавить функцию сохранения вывода; оформить загрузку и выгрузку в поле в меню;подготовить презентацию
-####################################
+"""
+изначальный набор комманд по ТЗ:
+1 загрузить список в панель
+2 отсортировать список пузырьком
+3 отсортировать список выбором
+4 отсортировать список вставками
+5 вывести полученый список в панель
+0 выход
+"""
+
+"""TODO pep 257; 
+расширить загрузку файла, чтоб можно было подавать списки и через пробел;
+добавить функцию сохранения вывода; 
+оформить загрузку и выгрузку в поле в меню;подготовить презентацию"""
+
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
-from mysorts import *
+
+from mysorts import bubble_sort, selection_sort, insertion_sort
+
+
+lst = []        # Глобальный спискок
+
 
 def  my_sort(lst):
     if lst:
-        if sort_type.get() =='bubble':
+        if sort_type.get() == 'bubble':
 
             bubble_sort(lst)
             debug_display_var.set("список отсортирован пузырьком")
@@ -32,19 +42,24 @@ def  my_sort(lst):
     else:
         debug_display_var.set('список пуст')
 
+
 def load_lst(*args):
     
     filename = askopenfilename()
     file = open(filename,'r')
+    
+    global lst
+    lst = []
 
     try:
-        global lst
-        lst=[]
         for line in file:
             lst.extend(list(map(int, line.split(","))))
-        debug_display_var.set("список загружен")
-    except:
+    except ValueError:
         debug_display_var.set("нечитаемый файл")
+    else:
+        debug_display_var.set("список загружен")
+    file.close()
+
 
 def update_display(*args):
     if lst:
@@ -56,84 +71,106 @@ def update_display(*args):
         debug_display_var.set("список пуст")
 
 
-lst = []#глобальный лист
 
-#определяем элементы окна
+# Определяем элементы окна
+
+# Основное окно
 root = Tk()
 root.title("Сортировка числовых списков")
-
+# Основная рамка, в которой находятся все элементы
 mainframe = ttk.Frame(root, padding=12,)
 
-#малый дисплей
+# Малый дисплей, выводящий доп информацию
 debug_display_var = StringVar()
 debug_display_var.set("загрузите список")
-debug_display =ttk.Label(mainframe, textvariable=debug_display_var, wraplength=200)
+debug_display = ttk.Label(
+    mainframe, textvariable=debug_display_var, wraplength=200)
 
-#загрузка списка из файла
-load_button = ttk.Button(mainframe, text='загрузить список из файла', command=load_lst)
+# Кнопка загрузки списка из файла TODO переделать в меню
+load_button = ttk.Button(
+    mainframe, text='загрузить список из файла', command=load_lst)
 
-#главный дисплей
+# Главный дисплей, отвечающий за вывод списка
 main_display_var = StringVar()#переменная дисплея
 main_display_var.set("")
-#wraplength указан константой, что не очень хорошо, но я без понятия, как сделать его по требуемой минимальной ширине
-main_display = ttk.Label(mainframe, textvariable=main_display_var, borderwidth=8, relief='ridge', wraplength=340, justify='center',anchor='center')
+#wraplength указан константой, что не очень хорошо
+#но я без понятия, как сделать его по требуемой минимальной ширине
+main_display = ttk.Label(
+    mainframe, textvariable=main_display_var, 
+    borderwidth=8, relief='ridge', wraplength=340, 
+    justify='center',anchor='center')
 
-#печать на дисплей
+# Обработка печати на дисплей
+# Галочка, отвечающая за длинну выводимого списка
 print_limited_state = BooleanVar(value=False)
-print_limited_check = ttk.Checkbutton(mainframe, text="напечатать только первые 20 эмелентов", variable=print_limited_state)
+print_limited_check = ttk.Checkbutton(
+    mainframe, text="напечатать только первые 20 эмелентов",
+    variable=print_limited_state)
+# Кнопка печати на главный дисплей
+print_button = ttk.Button(
+    mainframe, text='напечатать список', command=update_display)
 
-print_button = ttk.Button(mainframe, text='напечатать список', command=update_display)
-
-#выбор и сортировка списка
-
+# Выбор и запуск сортировки списка
+# Выбор сотрировки
 sort_type = StringVar(value='bubble')
+sort_radio_bubble = ttk.Radiobutton(mainframe, 
+    text="сортировка пузырьком", variable=sort_type, value='bubble' )
+sort_radio_selection = ttk.Radiobutton(mainframe, 
+    text="сортировка выбором", variable=sort_type, value='selection' )
+sort_radio_insertion = ttk.Radiobutton(mainframe, 
+    text="сортировка вставками", variable=sort_type, value='insertion' )
+# Кнопка сортировки
+sort_button = ttk.Button(mainframe, 
+    text="отсортировать", command=lambda: my_sort(lst))
 
-sort_radio_bubble = ttk.Radiobutton(mainframe, text="сортировка пузырьком", variable=sort_type, value='bubble' )
-sort_radio_selection = ttk.Radiobutton(mainframe, text="сортировка выбором", variable=sort_type, value='selection' )
-sort_radio_insertion = ttk.Radiobutton(mainframe, text="сортировка вставками", variable=sort_type, value='insertion' )
+# Кнопка выхода
+exit_button = ttk.Button(mainframe, 
+    text="выход", command=lambda: root.destroy())
 
-sort_button = ttk.Button(mainframe, text="отсортировать", command=lambda: my_sort(lst))
 
-#кнопка выхода TODO
-exit_button = ttk.Button(mainframe, text="выход", command=lambda: root.destroy())
+# Расставляем элементы
 
-#расставляем элементы
-
+# Основная рамка на всё окно
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-#дебаг-дисплейчик
+# Дебаг-дисплей слева сверху
 debug_display.grid(column=0, row=0)
-#кнопка загрузки в самом верху
-load_button.grid(column=1, row=0) #заменить на column=0, row=0, columnspan=2
-#дисплей под ней, на всю ширину и высоту
+# Кнопка загрузки сверху справа TODO переделать в меню
+load_button.grid(column=1, row=0)
+# Главный дисплей под ними, на всю ширину и высоту
 main_display.grid(column=0, row=1, columnspan=2, sticky='nwes')
 
-#элементы печати, левая часть внизу
+# Элементы печати списка на дисплей, левая часть внизу
 print_limited_check.grid(column=0, row=2)
 print_button.grid(column=0, row=3)
 
-#кнопка выхода в самом низу слева
+# Кнопка выхода в самом низу слева
 exit_button.grid(column=0, row=5, sticky=W)
 
-#элементы сортировки, правая часть внизу
+# Элементы сортировки, правая часть внизу
 sort_radio_bubble.grid(column=1, row=2, sticky=W)
 sort_radio_selection.grid(column=1, row=3, sticky=W)
 sort_radio_insertion.grid(column=1, row=4, sticky=W)
 sort_button.grid(column=1, row=5)
 
-#растяигивание элементов?
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-#паддинг для всех элементов
+# Паддинг для всех элементов в рамке
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
-root.bind('<x>', lambda e: debug_display_var.set(str(root.winfo_width())+" "+ str(root.winfo_height())+" ; " + str(root.winfo_reqwidth())+" "+ str(root.winfo_reqheight()) ))
+# Кнопка <x> выводит текущие размеры окна 
+# и требуемые размеры окна в малый дисплей
+root.bind('<x>', 
+    lambda e: debug_display_var.set(
+        str(root.winfo_width())+" "
+        + str(root.winfo_height())+" ; " + str(root.winfo_reqwidth())+" "
+        + str(root.winfo_reqheight()))
+)
+
+# Кнопка l (L) выводит длинну хранимого списка
 root.bind('<l>', lambda e: debug_display_var.set(str(len(lst))))
 
+# Запрет изменения размера окна
 root.resizable(False, False)
 
-#root.maxsize(x,y)
-
+# Основной ивент окна
 root.mainloop()
